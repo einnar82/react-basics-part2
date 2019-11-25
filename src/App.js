1f5989aec508f4c902749d058f2626f6406d7cec
Rannie Ollit <einnar82@gmail.com>
2019-11-25 16:16:57 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      middleName: "",
+      lastName: "",
+      age: ""
+    };
+  }
+
+  handleChange = evt => {
+    evt.persist();
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <p>my first is {this.state.firstName}</p>
+        {Object.keys(this.state).map((keyName, index) => (
+          <div key={index}>
+            {keyName}:{" "}
+            <input type="text" name={keyName} onChange={this.handleChange} />
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <header>this is header</header>;
+const Footer = () => <footer>this is footer</footer>;
+export { Header, Footer };
